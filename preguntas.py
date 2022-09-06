ed6cf378aa47f4e549c2ec8e664867514f174bb7
Juan Pablo Diaz Cervantes <53982528+jjpdc@users.noreply.github.com>
2022-09-06 11:41:13 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -57,8 +57,8 @@ def pregunta_02():
     diccionario = collections.Counter(punto2)
     dict1 = diccionario.items()
     for x, y in dict1:
-      print(str(x) + ',' + str(y))
-    return
+      fin=(str(x) + ',' + str(y))
+    return fin
 
 
 def pregunta_03():
